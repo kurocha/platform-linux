d8c34c3f80fbdf6a65460ed7b49fd4bca98dcd4e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-12-08 20:52:06 +1300

    Add a few useful provisions.

--- teapot.rb
+++ teapot.rb
@@ -25,4 +25,18 @@ define_target "linux" do |target|
 	end
 	
 	target.provides :platform => "Platform/linux"
+	
+	target.depends "Aggregate/OpenGL/Headers"
+	
+	target.provides "Library/OpenGL" do
+		append linkflags "-lGL"
+	end
+	
+	target.provides "Library/OpenAL" do
+		append linkflags "-lAL"
+	end
+	
+	target.provides "Language/C++11" do
+		cxxflags %W{-std=c++11 -Wno-c++11-narrowing}
+	end
 end
